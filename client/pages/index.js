12c9134b0c36c128af2b08dd6fe5e91e1cdf582e
Anushka Agrawal <agrawal.anushka206@gmail.com>
2025-11-08 17:51:20 +0530

    Incoming client/Server requests

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,19 +1,37 @@
-import axios from "axios";
+import buildClient from "../api/build-client";
 
 const LandingPage = ({ currentUser }) => {
-    console.log(currentUser);
-    // Request to fetch data made via browser/client
-    axios.get('/api/users/currentuser').catch((err) => {
-        console.log(err.message);
-    });
-    return <h1>Landing Page</h1>;
+    return currentUser ? <h1>You are signed in</h1> : <h1>You are NOT signed in</h1>
+    // console.log(currentUser);
+    // // // Request to fetch data made via browser/client
+    // // axios.get('/api/users/currentuser').catch((err) => {
+    // //     console.log(err.message);
+    // // });
+    // return <h1>Landing Page</h1>;
 };
 
 // Because of difference in enviornment, the request gives error when made via server
-// LandingPage.getInitialProps = async () => {
-//     const response = await axios.get('/api/users/currentuser');
-
-//     return response.data;
-// }
+LandingPage.getInitialProps = async (context) => {
+    const client = buildClient(context);
+    const { data } = await client.get('/api/users/currentuser');
+    return data;
+    // // console.log(req.headers);
+    // if (typeof window === 'undefined') {
+    //     // we are on the server!
+    //     // requests should be made to http://ingress-nginx-controller
+    //     const { data } = await axios.get(
+    //         'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser', {
+    //         headers: req.headers
+    //     }
+    //     );
+    //     return data;
+    // } else {
+    //     // we are on the browser!
+    //     // requests can be made with base url of ''
+    //     const { data } = await axios.get('/api/users/currentuser');
+    //     return data;
+    // }
+    // return {};
+}
 
 export default LandingPage;
