9970daccab42f8df477a9c3237c049e1ebb5b3a2
Anushka Agrawal <agrawal.anushka206@gmail.com>
2026-01-11 16:24:30 +0530

    Mongo Connection

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -10,10 +10,13 @@ const start = async () => {
   if (!process.env.JWT_KEY) {
     throw new Error("JWT_KEY must be defined");
   }
+  if (!process.env.MONGO_URI) {
+    throw new Error("MONGO_URI must be defined");
+  }
   // Write name of service, than the port, than the db name
   // DataBase, will be created automatically - auth named
   try {
-    await mongoose.connect("mongodb://auth-mongo-srv:27017/auth");
+    await mongoose.connect(process.env.MONGO_URI);
     console.log("Connected to MongoDB");
   } catch (err) {
     console.error(err);
