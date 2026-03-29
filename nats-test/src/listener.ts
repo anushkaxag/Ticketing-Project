5554cacf74f1ead2075254fccb87c0c25aad9440
Anushka Agrawal <agrawal.anushka206@gmail.com>
2026-03-29 12:20:05 +0530

    Listener Config

--- nats-test/src/listener.ts
+++ nats-test/src/listener.ts
@@ -10,12 +10,34 @@ const stan = nats.connect("ticketing", randomBytes(4).toString("hex"), {
 stan.on("connect", () => {
   console.log("Listener connected to NATS");
 
-  const subscription = stan.subscribe("ticket:created");
+  stan.on("close", () => {
+    console.log("NATS connection closed!");
+    process.exit();
+  });
+
+  // setManualAckMode(true): Manual ack required for incoming event
+  const options = stan
+    .subscriptionOptions()
+    .setManualAckMode(true)
+    .setDeliverAllAvailable()
+    .setDurableName("accounting-service");
+
+  // second argument is the name of the Queue Group current listener is joining
+  const subscription = stan.subscribe(
+    "ticket:created",
+    "orders-service-queue-group",
+  );
 
   subscription.on("message", (msg: Message) => {
     const data = msg.getData();
     if (typeof data === "string") {
       console.log(`Recieved event #${msg.getSequence()}, with data: ${data}`);
     }
+
+    msg.ack();
   });
 });
+
+// Watches for interrupt or terminate signals
+process.on("SIGINT", () => stan.close());
+process.on("SIGTERM", () => stan.close());
